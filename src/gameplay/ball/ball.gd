493caff91788246ea37d3cc14149c45650996b1f
is386 <singh.indervir89@gmail.com>
2026-08-09 15:57:11 -0400

    Add signal for when ball exits screen

--- src/gameplay/ball/ball.gd
+++ src/gameplay/ball/ball.gd
@@ -1,15 +1,21 @@
 class_name Ball
 extends CharacterBody2D
 
+signal scored(is_player_one: bool)
+
 const MAX_SPEED_MULTIPLIER = 2
 
 @export var speed: float = 100
 
+@onready var visible_notifier: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
+
 var _speed_multiplier: float = 1
+var _last_player_collided: Player = null
 
 
 func _ready() -> void:
 	velocity = Vector2(speed, 0)
+	visible_notifier.screen_exited.connect(_on_screen_exited)
 
 
 func _physics_process(delta: float) -> void:
@@ -21,6 +27,7 @@ func _physics_process(delta: float) -> void:
 
 	if collision.get_collider() is Player:
 		var player := collision.get_collider() as Player
+		_last_player_collided = player
 		var velocity_bounce := velocity
 
 		velocity = global_position - player.global_position
@@ -29,3 +36,8 @@ func _physics_process(delta: float) -> void:
 
 		_speed_multiplier = min(MAX_SPEED_MULTIPLIER, _speed_multiplier + 0.1)
 		velocity.x *= _speed_multiplier
+
+
+func _on_screen_exited() -> void:
+	scored.emit(_last_player_collided.is_player_one)
+	queue_free()
